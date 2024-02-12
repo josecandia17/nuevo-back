7c573627a61f53cb06d26f5a9f5627f7edde4735
jose candia <carlosjose171509@gmail.com>
2024-02-12 19:32:10 -0400

    relacion modelos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+
+    public function pedidos(){
+        return $this ->hasMany(Pedido::class)->withPivot(["cantidad"])-> withTimestamps();
+    }
+
+    public function cliente(){
+        return $this ->hasMany(Cliente::class);
+    }
 }
--- app/Models/Podructo.php
+++ app/Models/Podructo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Podructo extends Model
 {
     use HasFactory;
+
+    public function categoria(){
+        return $this ->hasMany(Categoria::class);
+    }
+
+    public function pedidos(){
+        return $this ->hasMany(Pedido::class)->withPivot(["cantidad"])-> withTimestamps();
+    }
 }

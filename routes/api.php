856c5895e8e0779f34fd3f19b085043ea72aed10
jose candia <carlosjose171509@gmail.com>
2024-02-19 12:25:06 -0400

    CATEGORIA

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -19,3 +20,5 @@ Route::prefix('/v1/auth')->group(function () {
         Route::post('/logout', [AuthController::class, "funSalir"]);
     });
 });
+
+Route::apiResource("categoria", CategoriaController::class);
